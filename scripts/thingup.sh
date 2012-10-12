e7b3bb9c28534fa2cd7ddc8c1a4f161a3b830ae4
Philippe Breuils <philippe.breuils@gmail.com>
2012-10-12 16:07:15 +0200

    added paths for commands in sample script

--- scripts/thingup.sh
+++ scripts/thingup.sh
@@ -2,10 +2,10 @@
 uri=$1
 key=$2
 
-private_ip=`ifconfig eth0|xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/'`
+private_ip=`/sbin/ifconfig eth0|/usr/bin/xargs|/usr/bin/awk '{print $7}'|/bin/sed -e 's/[a-z]*:/''/'`
 echo "Found private IP address: $private_ip"
 
-public_ip=`curl http://myip.dnsomatic.com`
+public_ip=`/usr/bin/curl http://myip.dnsomatic.com`
 echo "Found public IP address: $public_ip"
 echo "Posting to $uri?key=$2"
 
